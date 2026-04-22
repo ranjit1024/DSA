0930234d671fcc16112f171cd73f746e3d1280d3
ranjit1024 <ranjitdas2048@gmail.com>
2026-04-22 20:10:42 +0530

    added new

--- 1.cpp
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-using namespace std;
-int main(){
-
-    printf("data");
-    return 0;
-}
---
+++ Daily.cpp
@@ -0,0 +1,25 @@
+// # include <iostream>
+
+// using namespace std;
+
+// int main(){
+// 	int i =2; 
+// 	while(i <= 10){
+// 		cout << i;
+// 		i += 2;
+// 	}
+
+// }
+
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	char ch = 'A';
+	while(ch < 'Z'){
+		cout << ch;
+		ch ++;
+	}
+}
