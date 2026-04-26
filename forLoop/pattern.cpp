272eaab58c8d254ec60bff1416cf57c35007d3aa
ranjit1024 <ranjitdas2048@gmail.com>
2026-04-26 23:01:34 +0530

    added triangle

--- forLoop/pattern.cpp
+++ forLoop/pattern.cpp
@@ -5,10 +5,10 @@ int main(){
     int n;
     cin >> n;
 
-    for(int i = 0; i <= n; i++){
-        for(int j = 0; j <= 10; j++){
-            cout << "* ";
+    for(int i = n; i >= 0; i--){
+        for(int j = 1; j <= i; j++){
+            cout << "*";
         }
-        cout << endl;
+        cout << endl; 
     }
 }
