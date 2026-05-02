1c4aee819ebb7ccf0b98a3c18ae6066d809e1926
ranjit1024 <ranjitdas2048@gmail.com>
2026-05-02 17:48:51 +0530

    added pattern printing

--- patternPrinting/first.cpp
+++ patternPrinting/first.cpp
@@ -2,13 +2,20 @@
 using namespace std;
 
 int main(){
-    int n; 
+    int n, m; 
     cin >> n;
-    
-    for(int i = 0; i < n; i++){
-        for(int j = 0; j < n; j++){
-            cout << "*";
+    cin >> m;
+    for(int i = 1; i <= n; i++){
+        for(int j = 1; j <= m; j++){
+            if(i == 1 or i == n or j == 1 or j==m){
+                cout << "*";
+            }
+            else{
+                cout << " ";
+            }
+
         }
         cout << endl;
     }
+  
 }
