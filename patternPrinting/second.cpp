96627c40b9c72842c6267858f178cfc468a089b6
ranjit1024 <ranjitdas2048@gmail.com>
2026-05-03 10:05:51 +0530

    added traiange

--- patternPrinting/second.cpp
+++ patternPrinting/second.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main(){
     int n;
     cin  >> n;
-    for(int i = 1; i<= n; i++){
+    for(int i = n; i >= 1; i--){
         for(int j = 1; j <= i ; j++){
             cout << "*";
         }
