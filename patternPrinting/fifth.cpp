8293f240494dedc123a32d01392300d81fd14ef6
ranjit1024 <ranjitdas2048@gmail.com>
2026-05-04 23:10:56 +0530

    added triange4

--- patternPrinting/fifth.cpp
+++ patternPrinting/fifth.cpp
@@ -5,16 +5,15 @@ using namespace std;
 int main(){
     int n; 
     cin >> n;
-    for(int i = 1; i <= n;i ++){
-        for(int j = 1; j <= n;  j++){
-            if(j <= n-i){
-                cout << " ";
-            }
-            else{
-
-                cout << "* ";
-            }
-        }
-        cout  << endl;
+    for(int i = 1; i <= n; i++){
+    
+    for(int j = 1; j <= n-i; j++){
+        cout << " ";
     }
+
+    for(int j = 1; j <= i; j++){
+        cout << "* ";
+    }
+    cout << endl;
+}
 };
