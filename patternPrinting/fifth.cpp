892f968386b9e535967816a1e4ca47b2f1721ac1
ranjit1024 <ranjitdas2048@gmail.com>
2026-05-04 22:53:58 +0530

    added tringale prat -4

--- patternPrinting/fifth.cpp
+++ patternPrinting/fifth.cpp
@@ -6,9 +6,15 @@ int main(){
     int n; 
     cin >> n;
     for(int i = 1; i <= n;i ++){
-        for(int j = 1; j <= i;  j++){
-            cout << " *";
+        for(int j = 1; j <= n;  j++){
+            if(j <= n-i){
+                cout << " ";
+            }
+            else{
+
+                cout << "* ";
+            }
         }
-        cout << endl;
+        cout  << endl;
     }
-}
+};
