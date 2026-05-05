42cf882ed696753d1d5e2bbcf14d3ef4a9019f05
ranjit1024 <ranjitdas2048@gmail.com>
2026-05-05 22:20:36 +0530

    added pattern

--- patternPrinting/fifth.cpp
+++ patternPrinting/fifth.cpp
@@ -1,19 +1,29 @@
-# include <iostream>
-
+#include <iostream>
 using namespace std;
 
-int main(){
-    int n; 
+int main() {
+    int n;
     cin >> n;
-    for(int i = 1; i <= n; i++){
-    
-    for(int j = 1; j <= n-i; j++){
-        cout << " ";
-    }
 
-    for(int j = 1; j <= i; j++){
-        cout << "* ";
+    for(int i = 1; i <= n; i++) {
+
+       
+        for(int j = 1; j <= n - i; j++) {
+            cout << " ";
+        }
+
+       
+        for(int j = 1; j <= i; j++) {
+            if(j == 1 or j == i){
+                cout << "*";
+            }
+            else{
+
+                cout << "*";
+            }
+            if(j < i) cout << " ";  // only between stars
+        }
+
+        cout << endl;
     }
-    cout << endl;
 }
-};
