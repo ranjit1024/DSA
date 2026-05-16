f7dd610e7fa4dfc085150811b2e51ff1083d959b
ranjit1024 <ranjitdas2048@gmail.com>
2026-05-16 15:32:55 +0530

    added good counter

--- contest/countgodnumber.cpp
+++ contest/countgodnumber.cpp
@@ -4,18 +4,20 @@
 using namespace std;
 
 int main(){
-   long long int n;
+    int n;
    cin >> n;
    int ans = 0;
    vector<long long> x(n);
-   for(int i = 0; i <= n; i++){
+   for(int i = 0; i < n; i++){
     cin >> x[i];
    }
   
-   for(int i = 0; i <= n ; i++){
-    if(18 % x[i] == 0 or x[i] % 45 == 0){
-      ans = ans +  1;
-
+   for(int i = 0; i < n ; i++){
+    if(x[i] % 45 == 0){
+      ans++;
+    }
+    else if(x[i] != 0 and 18 % x[i] == 0){
+      ans++;
     }
    }
    cout << ans;
