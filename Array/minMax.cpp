f6a1d31db5d0991a9b4ede5b3e104a6c6a6b5754
ranjit1024 <ranjitdas2048@gmail.com>
2026-05-23 23:32:47 +0530

    added today prpgress

--- Array/minMax.cpp
+++ Array/minMax.cpp
@@ -2,5 +2,24 @@
 using namespace std;
 
 int main(){
-    cout << "hellow Workd";
+    int n;
+    cin >> n;
+    int array[n];
+    int min = 0;
+    int counter = 0;
+    for (int i  = 0; i < n; i++){
+        cin >> array[i];
+    }
+    for(int i = 0 ; i < n; i++){
+        if(array[counter] > array[i]){
+            min = array[i];
+            
+        }
+        else{
+            min = array[counter];
+        }
+    }
+    cout << endl;
+    cout <<  min << endl;
+
 }
