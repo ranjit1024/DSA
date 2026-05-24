5bd93e054674e326824d4d05a3e9559c6acaa0a3
ranjit1024 <ranjitdas2048@gmail.com>
2026-05-24 10:05:18 +0530

    added min in array

--- Array/minMax.cpp
+++ Array/minMax.cpp
@@ -4,22 +4,23 @@ using namespace std;
 int main(){
     int n;
     cin >> n;
-    int array[n];
+    long long array[n];
     int min = 0;
     int counter = 0;
+    int index = 1;
     for (int i  = 0; i < n; i++){
         cin >> array[i];
     }
     for(int i = 0 ; i < n; i++){
         if(array[counter] > array[i]){
             min = array[i];
-            
+            index = i;
         }
         else{
             min = array[counter];
         }
     }
-    cout << endl;
     cout <<  min << endl;
+    cout << (index + 1 ) << endl;
 
 }
