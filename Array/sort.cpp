21396f8212063c66af5242dea7ab757a6d88bb76
ranjit1024 <ranjitdas2048@gmail.com>
2026-05-24 23:01:24 +0530

    trying to solve arrray is sorted or not

--- Array/sort.cpp
+++ Array/sort.cpp
@@ -2,5 +2,28 @@
 using namespace std;
 
 int main(){
-    
+    long long  n;
+    cin >> n;
+
+    long long array[n];
+    long long  ans[n];
+    for (int i = 0; i < n; i++){
+        cin >> array[i];
+    }
+
+    for(int i = 0; i < n ; i++){
+        if(3 + array[i] == 3){
+            ans[i] = array[i];
+        }
+        else if(3 + array[i] == 4){
+            ans[i] = array[i];
+        }
+    }
+
+    for(int i = 0;  i < n; i++){
+        cout << ans[i];
+    }
+    cout << endl;
+
+
 }
