f0589e66c94f2579ac9245491d4605349315abed
ranjit1024 <ranjitdas2048@gmail.com>
2026-05-28 23:02:23 +0530

    added arage test case pass

--- Array/arrange.cpp
+++ Array/arrange.cpp
@@ -2,11 +2,38 @@
 using namespace std;
 
 int main(){
+    int x;
+    cin >> x;
+    while(x --){
+
+    
     int n;
     cin >> n;
 
-    int array[n];
-    for(int i = 1 ; i <= n; i = i +2){
-        cout << i << " " ;
+    long long array[n];
+    long long asn[n];
+
+    int odd = 0;
+    for(int i = 0 ; i < n ; i++){
+        array[i] = i + 1;
     }
+    for(int i = 0 ; i < n; i++){
+        if(array[i] % 2 != 0){
+            asn[odd] = array[i];
+            odd++;
+        } 
+    }
+    for(int i = n - 1; i >=0 ; i--){
+        if(array[i] % 2 == 0){
+            asn[odd] = array[i];
+            odd++;
+        }
+        
+    }
+
+    for(int i = 0; i < n ; i++){
+        i < n-1 ? cout << asn[i] << " " :  cout << asn[i];
+    } 
+    cout << endl;
+}
 }
