242c3a8a51397adb387779296d56202702c3c13a
ranjit1024 <ranjitdas2048@gmail.com>
2026-05-28 22:11:39 +0530

    added arrage

---
+++ Array/arrange.cpp
@@ -0,0 +1,12 @@
+# include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+
+    int array[n];
+    for(int i = 1 ; i <= n; i = i +2){
+        cout << i << " " ;
+    }
+}
