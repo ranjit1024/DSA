b8c6adc96aa1db0278bf92dd0cc9c002e61bf0de
ranjit1024 <ranjitdas2048@gmail.com>
2026-05-31 21:43:06 +0530

    added normal swap

--- Array/swap.cpp
+++ Array/swap.cpp
@@ -1,12 +1,40 @@
-# include <iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
-    int x;
-    cin >> x;
+int main()
+{
 
-    int n;
+    int t;
+    cin >> t;
+    while (t--)
+    {
+    long long n;
     cin >> n;
 
-    
+    long long array[n];
+    long long ans[n];
+    int inc = 0;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> array[i];
+    }
+
+    for (int i = 0; i < n; i  = i + 2)
+    {
+        if(i + 1 < n){
+            ans[inc] = array[i+1];
+            inc++;
+            ans[inc] = array[i];
+            inc ++;
+        }
+        else{
+            ans[inc++] = array[i];
+        }
+        
+    }
+
+    for(int i= 0; i < n; i++){
+        cout << ans[i] << " ";
+    }
+}
 }
