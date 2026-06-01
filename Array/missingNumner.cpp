0fbaec26b62f351da568ab66c25e9e1f277dea4b
ranjit1024 <ranjitdas2048@gmail.com>
2026-06-01 21:38:57 +0530

    addeidn missingn umber

--- Array/missingNumner.cpp
+++ Array/missingNumner.cpp
@@ -1,27 +1,35 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{
+int main() {
     int t;
     cin >> t;
-    while (t--)
-    {
+
+    while (t--) {
         int n;
         cin >> n;
 
-        int array[n];
-        for (int i = 0; i < n; i++)
-        {
+        long long array[n];
+
+        for (int i = 0; i < n; i++) {
             cin >> array[i];
         }
 
-        for(int i = 0 ; i < n; i++){
-            for(int j  = 0 ; i < n; j++){
-                cout << array[i];
+        for (int i = 0; i < n; i++) {
+            bool unique = true;
+
+            for (int j = 0; j < n; j++) {
+                if (i != j && array[i] == array[j]) {
+                    unique = false;
+                    break;
+                }
+            }
+
+            if (unique) {
+                cout << array[i] << endl;
+                break; 
             }
         }
         
-        cout << endl;
     }
 }
