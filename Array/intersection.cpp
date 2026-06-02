bc1c267822f368264675b4f7d9c7d5396a3aeefb
ranjit1024 <ranjitdas2048@gmail.com>
2026-06-02 21:42:53 +0530

    intersectino

--- Array/intersection.cpp
+++ Array/intersection.cpp
@@ -7,7 +7,25 @@ int main(){
 
     while (t--)
     {
+        int n;
+        cin >> n;
+
+        int m;
+        cin >> m;
+
+        int array[n];
+        int arary2[m];
+
+        for(int i = 0; i < n; i++){
+            cin >> array[i];
+        };
+
+        for(int i = 0; i < m; i++){
+            cin >> arary2[i];
+        };
+
         
+
     }
     
 }
