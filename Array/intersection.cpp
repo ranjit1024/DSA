ad0341c8acf30763e93b24869a88ac54f011200d
ranjit1024 <ranjitdas2048@gmail.com>
2026-06-03 22:46:38 +0530

    added todau

--- Array/intersection.cpp
+++ Array/intersection.cpp
@@ -1,31 +1,38 @@
-# include <iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
+int main()
+{
     int t;
     cin >> t;
 
     while (t--)
     {
+       
         int n;
         cin >> n;
 
-        int m;
-        cin >> m;
-
         int array[n];
-        int arary2[m];
-
-        for(int i = 0; i < n; i++){
+        for (int i = 0; i < n; i++)
+        {
             cin >> array[i];
         };
+        int m;
+        cin >> m;
+        int array2[m];
 
-        for(int i = 0; i < m; i++){
-            cin >> arary2[i];
+        for (int i = 0; i < m; i++)
+        {
+            cin >> array2[i];
         };
 
-        
-
+        for (int i = 0; i < n; i++)
+        {
+            if (array[i] == array2[i])
+            {
+                cout << array2[i];
+            }
+        }
+        cout << endl;
     }
-    
 }
