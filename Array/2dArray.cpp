867a9f078a381df1af7d481e85da2174ab4ec95f
ranjit1024 <ranjitdas2048@gmail.com>
2026-06-07 13:36:38 +0530

    added 2d array

--- Array/2dArray.cpp
+++ Array/2dArray.cpp
@@ -3,5 +3,13 @@
 using namespace std;
 
 int main(){
+    int n , m;
+    cin >> n;
+    cin >> m;
+
+    int array[n];
+    for(int i =0 ; i < n; i){
+        cin >> array[i];
+    }
     cout << "Hellow Wrold";
 }
