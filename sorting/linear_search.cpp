f937299cc5d809f263f1ad1b9d856c906f3766a7
ranjit1024 <ranjitdas2048@gmail.com>
2026-06-09 22:53:24 +0530

    add :liner search

--- sorting/linear_search.cpp
+++ sorting/linear_search.cpp
@@ -1,6 +1,25 @@
-# include <iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
+int main()
+{
+    int n;
+    cin >> n;
+    bool flag = false;
+    int Arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> Arr[i];
+    }
 
+    int x;
+    cin >> x;
+
+    for (int i = 0; i < n; i++)
+    {
+        if (x == Arr[i])
+        {
+            flag = true;
+        }
+    }
 }
