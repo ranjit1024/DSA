a63f1ecb60fa0fe78c7087567c7315dd2d377570
ranjit1024 <ranjitdas2048@gmail.com>
2026-06-11 19:11:20 +0530

    update:binery search

---
+++ sorting/binery_search.cpp
@@ -0,0 +1,22 @@
+# include <iostream>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    int l = 0;
+    int r = n;
+    int mid = (l + r) / 2;
+    
+    int Arr[n];
+    for(int i= 0 ;i < n; i++){
+        cin >> Arr[i];
+    }
+
+    int x;
+    cin >> x;
+
+
+    cout << "Hello World";
+}
