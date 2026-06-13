f65fb7cc0c66ec1fe08134413dba72263f90cbf7
ranjit1024 <ranjitdas2048@gmail.com>
2026-06-13 22:04:05 +0530

    my underdting of bs

--- sorting/binery_search.cpp
+++ sorting/binery_search.cpp
@@ -1,22 +1,32 @@
-# include <iostream>
+#include <iostream>
 
 using namespace std;
 
-int main(){
+int main()
+{
     int n;
     cin >> n;
     int l = 0;
     int r = n;
     int mid = (l + r) / 2;
-    
+    bool flag = false;
     int Arr[n];
-    for(int i= 0 ;i < n; i++){
+    for (int i = 0; i < n; i++)
+    {
         cin >> Arr[i];
     }
 
     int x;
     cin >> x;
 
-
-    cout << "Hello World";
+    while(Arr[mid] != x){
+        if(Arr[x] < Arr[mid]){
+            r = mid - 1;
+            mid = (l + r) / 2;
+        }
+        else{
+            l = mid + 1;
+              mid = (l + r) / 2;
+        }
+    }
 }
