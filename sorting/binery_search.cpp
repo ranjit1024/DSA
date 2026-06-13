f4f56900de3bf600d71899619df03749c8706891
ranjit1024 <ranjitdas2048@gmail.com>
2026-06-13 23:22:48 +0530

    aded biney search

--- sorting/binery_search.cpp
+++ sorting/binery_search.cpp
@@ -1,19 +1,40 @@
 // Array[mid] <  x move right
 // Array[mid] > x move left
 
-# include <iostream>
+#include <iostream>
 
 using namespace std;
 
-int main(){
+int main()
+{
     int n;
     cin >> n;
 
+
     int Arr[n];
-    for(int i = 0 ; i < n; i++){
+    for (int i = 0; i < n; i++)
+    {
         cin >> Arr[i];
     }
 
     int x;
     cin >> x;
+
+        int l = 0;
+    int r = n - 1;
+    while(l <= r ){
+        int mid = (r + l) / 2;
+        if(Arr[mid] == x){
+            cout << "Found at index " << mid;
+            break;
+        }
+        else if(Arr[mid] < x){
+            l = mid + 1;
+        }
+        else{
+            r = mid - 1;
+        }
+
+    }
+    cout << "Not found";
 }
