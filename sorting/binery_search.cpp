c6f7697ea8433709fccff9fb9f30019394b711a2
ranjit1024 <ranjitdas2048@gmail.com>
2026-06-13 23:15:51 +0530

    updatye

--- sorting/binery_search.cpp
+++ sorting/binery_search.cpp
@@ -1,32 +1,19 @@
-#include <iostream>
+// Array[mid] <  x move right
+// Array[mid] > x move left
+
+# include <iostream>
 
 using namespace std;
 
-int main()
-{
+int main(){
     int n;
     cin >> n;
-    int l = 0;
-    int r = n;
-    int mid = (l + r) / 2;
-    bool flag = false;
+
     int Arr[n];
-    for (int i = 0; i < n; i++)
-    {
+    for(int i = 0 ; i < n; i++){
         cin >> Arr[i];
     }
 
     int x;
     cin >> x;
-
-    while(Arr[mid] != x){
-        if(Arr[x] < Arr[mid]){
-            r = mid - 1;
-            mid = (l + r) / 2;
-        }
-        else{
-            l = mid + 1;
-              mid = (l + r) / 2;
-        }
-    }
 }
