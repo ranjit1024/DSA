596c62167bc09d4bf2ab2d962ae2d839948f668b
ranjit1024 <ranjitdas2048@gmail.com>
2026-06-15 22:07:42 +0530

    update:add seletion sort

--- sorting/selecton_sort.cpp
+++ sorting/selecton_sort.cpp
@@ -1,33 +1,36 @@
 # include <iostream>
 using namespace std;
-
-void swap(int &a, int &b){
-   int temp = a;
-   a = b;
-   b = temp;
+void swap(long long &a, long long &b){
+    long long temp = a;
+    a = b;
+    b = temp;
 }
-
 int main(){
     int n;
     cin >> n;
 
-    int Arr[n];
+    long long  Arr[n];
     for(int i = 0 ; i < n; i++){
         cin >> Arr[i];
     }
 
-    for(int i = 0; i < n; i++){
-        int mn = Arr[i];
+    for(int i = 0 ; i < n - 1; i++){
+        long long min = Arr[i];
         int loc = i;
         for(int j = i; j < n; j++){
-            if(Arr[j] < mn){
-                mn = Arr[j];
+            if(Arr[j] < min){
+                min = Arr[j]; 
                 loc = j;
             }
         }
         swap(Arr[i], Arr[loc]);
+        cout << "Pass " << i + 1 << ": ";
+        for(int k  = 0; k < n; k++){
+            cout << Arr[k] << " ";
+        }
+        cout << ", " << "min_selected = " << min; 
+        cout  << endl;
     }
-    for(int i =0; i < n; i++){
-        cout << Arr[i];
-    }
+
+
 }
