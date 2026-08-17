e21db59bede6c14f91430130e782197f66c23ce9
ranjit1024 <ranjitdas2048@gmail.com>
2026-08-17 22:17:28 +0530

    new added

--- first.cpp
+++
@@ -1,7 +0,0 @@
-# include <iostream>
-using namespace std;
-
-int main(){
-    int n = 1235;
-    cout << n % 10;
-}
--- forLoop/first.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main(){
-    cout << "For loop";
-    for(int i  = 0; i <= 100; i++){
-        cout << i << "\n";
-    }
-} 
