ec3d24a5a6c23f6bad972fcd2057d195960f8eba
ranjit1024 <ranjitdas2048@gmail.com>
2026-08-17 23:13:49 +0530

    udpate

--- logicBuilding/easy.cpp
+++ logicBuilding/easy.cpp
@@ -3,5 +3,13 @@ using namespace std;
 
 
 int main(){
-    cout << "Data is the king";
+    cout << "Data is the king" << endl;
+    int n;
+    cin >> n;
+    for(int i = n; i >= 1; i-- ){
+        for (int j = 1; j <= i; j++){
+            cout << "*";
+        }
+        cout << endl;
+    }
 }
