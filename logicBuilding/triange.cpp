077c7a144ae55ee080350498d5cc26c3f1963e09
ranjit1024 <ranjitdas2048@gmail.com>
2026-09-03 22:07:26 +0530

    udpate

--- logicBuilding/triange.cpp
+++ logicBuilding/triange.cpp
@@ -2,15 +2,14 @@
 using namespace std;
 
 int main(){
-    int n  =4;
-
-    for(int i  = 0; i < n; i++){
-        for(int j = 0; j < i; j++){
-            cout << " ";
-        }
-        for(int j = 0; j < n * 2 - (i * 2 + 1); j++){
-            cout << "*";
+    int n = 4;
+    char letter = 'A' + (n-1);
+    for(int i = 0; i < n; i++){
+        for(char ch = letter - i ; ch <= letter; ch++){
+            cout << ch;
         }
         cout << endl;
     }
+
+
 }
