31c7d76d16ecbff731f7db976be1f7714360b5f3
ranjit1024 <ranjitdas2048@gmail.com>
2026-09-09 23:16:58 +0530

    udpate:donw

--- stl/first.cpp
+++ stl/first.cpp
@@ -1,7 +1,21 @@
 #include<bits/stdc++.h>
+#include <ostream>
+#include <utility>
 using namespace std;
+void explainPair(){
+    pair <int, int> pr1 = {12,12};
+    cout << pr1.first <<  pr1.second << endl;
+}
+void explainVector(){
+    vector<int> vec;
+    vec.push_back(12);
+    vec.push_back(2);
+    vec.push_back(4);
 
+    cout << vec.size() << endl;
+}
 int main(){
     int res = pow(5, 2);
-    cout << res;
+    explainVector();
+
 }
