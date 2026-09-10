672de02f5ec0b945dcc7d3f2a0a1a6065a15acc7
ranjit1024 <ranjitdas2048@gmail.com>
2026-09-10 23:21:29 +0530

    update

--- stl/first.cpp
+++ stl/first.cpp
@@ -1,4 +1,5 @@
 #include<bits/stdc++.h>
+#include <iostream>
 #include <ostream>
 #include <utility>
 using namespace std;
@@ -12,11 +13,10 @@ void explainVector(){
     vec.push_back(2);
     vec.push_back(4);
 
-    // cout << vec.size() << endl;
+    //
     for(int i = 0; i < vec.size(); i++){
-        cout << vec[i] << endl;
+        cout << vec[i];
     }
-
 }
 int main(){
     int res = pow(5, 2);
