6ca14527531ddd542030530df033fc65c5824667
ranjit1024 <ranjitdas2048@gmail.com>
2026-09-14 13:35:41 +0530

    almost done with vec

--- stl/stl.cpp
+++ stl/stl.cpp
@@ -6,16 +6,16 @@ void vector_expain(){
     vector<int> nums;
     nums.push_back(1);
     nums.push_back(2);
+    nums.push_back(12);
+    nums.push_back(34);
 
-    auto vec_first = nums.begin();
-    auto vec_last = nums.end();
-
-
+    nums.erase(nums.begin());
     for(auto i : nums){
         cout << i << " ";
     }
 
 }
+
 int main(){
     vector_expain();
     return 0;
