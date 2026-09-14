7cc5c40b25207e51fdf8f7936c5807e5b85fa69d
ranjit1024 <ranjitdas2048@gmail.com>
2026-09-14 13:47:55 +0530

    added list

--- stl/stl.cpp
+++ stl/stl.cpp
@@ -1,22 +1,28 @@
 # include <bits/stdc++.h>
+#include <iostream>
 
 using namespace std;
-
+void pair_explain (){
+    pair<int, int> pair = {1,1};
+}
 void vector_expain(){
-    vector<int> nums;
-    nums.push_back(1);
-    nums.push_back(2);
-    nums.push_back(12);
-    nums.push_back(34);
+    vector<int> vec1 = {1,2,3,4};
+    vec1.insert(vec1.begin(), 21);
 
-    nums.erase(nums.begin());
-    for(auto i : nums){
+    for(auto i : vec1){
         cout << i << " ";
     }
-
+    cout << endl;
 }
-
+void explain_list(){
+    list<int>ls = {2,3};
+    ls.push_front(1);
+    for(auto l : ls){
+        cout << l;
+    }
+    cout << endl;
+}
 int main(){
-    vector_expain();
+    explain_list();
     return 0;
 }
